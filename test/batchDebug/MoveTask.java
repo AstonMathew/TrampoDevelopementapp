e93888e4bea2d5819ac6fd0613232d1ca214521a
Guillaume Jolly <gui@trampocfd.com>
2017-04-05 18:04:13 +1000

    for real time file move to sync folder

---
+++ test/batchDebug/MoveTask.java
@@ -0,0 +1,64 @@
+package batchDebug;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+
+public class MoveTask {
+    private final static int PERIOD = 1; // hours
+
+    private Timer cron;
+    private File source;
+    private File destination;
+
+    public MoveTask(File source, File destination) {
+        this.source = source;
+        this.destination = destination;
+        cron = new Timer();
+    }
+
+    public void scheduleFileMove(String string) {
+        TimerTask moveTimerTask = new MoveTimerTask(source, destination, string);
+        cron.schedule(moveTimerTask, 1, TimeUnit.SECONDS.toMillis(PERIOD));
+    }
+    public void cancelPurgeTimer() {
+        cron.cancel();
+        cron.purge();
+    }
+    
+
+    class MoveTimerTask extends TimerTask {
+        File source;
+        File destination;
+        String string;
+
+        public MoveTimerTask(File source, File destination, String string) {
+            this.source = source;
+            this.destination = destination;
+            this.string = string;
+            System.out.println(String.format("Move task for mask '%s' is scheduled to run every %d hour(s)", string, PERIOD));
+        }
+
+        @Override
+        public void run() {
+            try {
+                File[] directoryListing = source.listFiles();
+                if (directoryListing != null) {
+                    for (File child : directoryListing) {
+                        if (Files.isRegularFile(child.toPath(), LinkOption.NOFOLLOW_LINKS) && child.getName().toLowerCase().contains(string.toLowerCase())) {
+                            System.out.println("directoryListing child.getName = " + child.getName());
+                            Files.move(child.toPath(), destination.toPath().resolve(child.getName()));
+                            System.out.println(" directoryListing child moved to  = " + destination.toPath().resolve(child.getName()));
+                        }
+                    }
+                }
+            } catch (
+                    IOException ignore) {
+            }
+        }
+    }
+}
