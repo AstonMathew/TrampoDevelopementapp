d818058e56b0c539aacc80c1564c9f6e184ea68a
Guillaume Jolly <Guillaume Jolly@DESKTOP-ASSEK32>
2017-06-07 15:01:27 +1000

    updated to handle csv folder directly in src folder

--- src/scan4macro/Scan.java
+++ src/scan4macro/Scan.java
@@ -6,6 +6,17 @@ public class Scan extends TypesExtractor{
 
     public Scan(File f) {
       super(f);
+      File csvFiles = new File("src\\csv"); // open csv path 
+      File[] csvs = csvFiles.listFiles();
+      for( int i=0; i<csvs.length; i++ ) {
+              if( !csvs[i].getName().endsWith(".csv") ) // check if not csv file
+                  continue;
+              if( csvs[i].getName().contains("whitelist") ) { // if filename has whitelist in its file name then it's package declaration csv
+                   new WhitePackages(csvs[i].getPath());
+              }else {
+                   new WhiteClasses(csvs[i].getPath(),csvs[i].getName().replace("csv", ""));
+              }
+      }
     }
      
     public boolean scan(){
