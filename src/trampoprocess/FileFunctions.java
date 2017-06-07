c62d2df4029445c64810aa0ee08ed1c7835ba33a
Guillaume Jolly <Guillaume Jolly@DESKTOP-ASSEK32>
2017-06-07 17:26:43 +1000

    logging setup

--- src/trampoprocess/FileFunctions.java
+++ src/trampoprocess/FileFunctions.java
@@ -6,25 +6,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FileFunctions {
-	public static boolean fileIsAvailable(Path file) {
-	  return Files.isRegularFile(file);
-	}
-	
-	public static int countFiles(Path dir) {
-		int count = 0;
-		try {
-			Iterator<Path> fileIt = Files.list(dir).iterator();
-			while (fileIt.hasNext()) {
-				Path file = fileIt.next();
-				System.out.println("Going thorugh file " + file);
-				if (Files.isRegularFile(file)) {count ++ ;}
-			}
-		} catch (IOException e) {
-			System.out.println("Unable to count files in " + dir);
-			count = 0;
-		}
-		return count;
-	}
+
+    static final Logger LOG = LoggerFactory.getLogger(FileFunctions.class);
+
+    public static boolean fileIsAvailable(Path file) {
+        return Files.isRegularFile(file);
+    }
+
+    public static int countFiles(Path dir) {
+        int count = 0;
+        try {
+            Iterator<Path> fileIt = Files.list(dir).iterator();
+            while (fileIt.hasNext()) {
+                Path file = fileIt.next();
+                LOG.debug("Going through file " + file);
+                if (Files.isRegularFile(file)) {
+                    count++;
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("Unable to count files in " + dir);
+            count = 0;
+        }
+        return count;
+    }
 
 }
