63c432690425763f6527032fd066b51c6c4600fe
Guillaume Jolly <Guillaume Jolly@DESKTOP-ASSEK32>
2017-06-07 15:18:20 +1000

    ExceptionTest example

---
+++ test/fileVisitorExamples/ExceptionTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileVisitorExamples;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ExceptionTest {
+     // exception test
+//        try  {
+//        throw new Exception("test stack trace print to log");
+//        }
+//        catch(Exception e){
+//        LOG.error("test exception",e);
+//        }
+}
