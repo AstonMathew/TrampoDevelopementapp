fc7007f10fc91adc6ed34c4923ea728a7d143eed
Guillaume Jolly <Guillaume Jolly@DESKTOP-ASSEK32>
2017-06-07 15:03:41 +1000

    added DevProdSwitch to instantly switch from PROD to DEV settings

---
+++ src/trampoprocess/DevProdSwitch.java
@@ -0,0 +1,43 @@
+package trampoprocess;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+// this fil is a SWITCH between PROD and DEV settings
+public class DevProdSwitch {
+  
+    public static final String environment = "PROD"; //DEV or PROD
+    Properties prop;
+    
+    public DevProdSwitch() {
+        prop = new Properties();
+        InputStream input = null;
+
+        try {
+            if(environment.equals("DEV")){
+              input = new FileInputStream("src/main/resources/dev.properties"); //text file with key,value for DEVELOPMENT settings
+            }else{
+              input = new FileInputStream("src/main/resources/prod.properties"); //text file with key,value for PRODUCTION settings
+            }
+            
+            // load a properties file
+            prop.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+  
+    public String getProperty(String key){
+      return prop.getProperty(key);
+    }
+}
